532f33135b962089dc78793db467c5e7a3c8d931
Brandon Furr <brandon.furr123@gmail.com>
2019-09-04 03:50:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  link_phrase = "It's Dangerous To Go Alone! Take This."
+  puts link_phrase
+end
+
+def any_phrase
+  phrase = "Do A Barrel Roll!"
+  puts phrase
+end 
